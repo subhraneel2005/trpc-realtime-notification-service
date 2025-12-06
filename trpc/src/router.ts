494b93bc56d784c779448cdd7d7a9e3c4aae9c04
subhraneel2005 <digitalsubhraneel@gmail.com>
2025-12-06 19:21:39 +0530

    chore/added missed trpc files

--- trpc/src/router.ts
+++ trpc/src/router.ts
@@ -1,8 +1,10 @@
 import { userRouter } from "./routers/user";
+import { wsRouter } from "./routers/ws";
 import { router } from "./trpc";
 
 export const appRouter = router({
   user: userRouter,
+  ws: wsRouter,
 });
 
 export type AppRouter = typeof appRouter;
---
+++ trpc/src/routers/ws.ts
@@ -0,0 +1,28 @@
+import z from "zod";
+import { privateProcedure, router } from "../trpc";
+
+const WS_SERVER_URL = process.env.WS_SERVER_URL || "ws://localhost:8080";
+
+export const wsRouter = router({
+  connectUserToWSS: privateProcedure
+    .input(
+      z.object({
+        type: z.string(),
+        userId: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const ws = new WebSocket(WS_SERVER_URL);
+      ws.onopen = () => {
+        ws.send(
+          JSON.stringify({
+            type: input.type,
+            userId: input.userId,
+          })
+        );
+      };
+      ws.onmessage = (event) => {
+        console.log("Message from trpc server ", event.data);
+      };
+    }),
+});
